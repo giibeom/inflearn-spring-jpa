807c3cf36675166976cede3c6f81aa0c89ecf15d
giibeom <kpmyung@gmail.com>
2022-12-26 22:11:38 +0900

    :sparkles: 회원 가입 화면을 개발하고 컨트롤러와 연결한다

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.member.Address;
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        // 데이터 값을 아무것도 넣지 않아도 Validation을 위해 깡통을 넣는 경우도 있음
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        // 첫번째 페이지로 넘어감
+        return "redirect:/";
+    }
+}
